db891941e12930f1826f8fb525892e36d62b8505
Manju KC <kcmanjuthapa@gmail.com>
2019-10-15 15:43:53 -0400

    added router and link to the app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import Login from './components/Login'
-import './App.css';
+import React, { Component } from "react";
+import Login from "./components/Login";
+import { Route, Link } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Login/>
+        <Link to="/login">Login</Link>
+        <Route path="/login" component={Login} />ˇ
       </div>
     );
   }
